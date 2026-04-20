b9799bc463d60ae53a4bcfe837b18316652cfc88
Zira Tech <zira-tech@example.com>
2026-04-20 12:35:56 +0300

    Add runtime nodejs to API routes

--- src/app/api/aircraft/route.ts
+++ src/app/api/aircraft/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export const dynamic = 'force-dynamic';
+export const runtime = 'nodejs';
 
 export async function GET() {
   try {
